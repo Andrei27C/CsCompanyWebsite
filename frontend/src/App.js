24418792615e24422bc5215d1e4f0e78c6606e5f
Csatlos Andrei <csatlos_andrei27@yahoo.com>
2022-05-23 11:40:37 +0300

    Final version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { Component }  from 'react';
-import { BrowserRouter as Router, HashRouter, Route, Routes } from 'react-router-dom';
+import React  from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
@@ -12,22 +11,7 @@ import Employee from "./components/Employee";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
       <Router>
           <Routes>
               <Route path="/" element={<Navigate replace to="/home" />} />
